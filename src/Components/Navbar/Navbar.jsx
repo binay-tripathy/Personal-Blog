5ee9d1570fc2f098210d427e703896ce2fbfc84a
Binay Tripathy <binaytripathy123@gmail.com>
2023-09-25 22:37:08 +0530

    Random commit
    Signed-off-by: Binay Tripathy <binaytripathy123@gmail.com>

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -9,22 +9,6 @@ const Navbar = () => {
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
-                <div className="collapse navbar-collapse" id="navbarNav">
-                    <ul className="navbar-nav ml-auto">
-                        <li className="nav-item">
-                            <a className="nav-link" href="/">Home</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="/">About</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="/">Blog</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="/">Contact</a>
-                        </li>
-                    </ul>
-                </div>
             </div>
         </nav>
     )
