4e7257cbd2562b2d58ce1bd01d016b358471deb0
Binay Tripathy <binaytripathy123@gmail.com>
2023-09-27 21:08:39 +0530

    Added all blog-posts
    Signed-off-by: Binay Tripathy <binaytripathy123@gmail.com>

--- src/Components/Blogs/Blogs.jsx
+++ src/Components/Blogs/Blogs.jsx
@@ -3,123 +3,109 @@ import './Blogs.css'
 import ewu from '../../Images/ends_with_us.jpg'
 import ry from '../../Images/regretting_you.png'
 import pap from '../../Images/PridePrejudice.jpg'
+import rs from '../../Images/Rishi_Sunak.png'
+import g20i from '../../Images/G20_India.png'
+import rk from '../../Images/rishikesh.jpg'
+import wk from '../../Images/WestKowloon.jpg'
+import ic from '../../Images/instantChili.jpg'
+import vcc from '../../Images/VeganCarrotCake.jpg'
 
 const Blogs = () => {
     return (
         <section className="blog-posts py-5">
             <div className="container">
                 <div className="row">
-                     {/* Blog Post 1 */}
                     <div className="col-md-4">
                         <div className="card mb-4">
-                            <img src={ewu} className="card-img-top" alt="post1"/>
-                                <div className="card-body">
-                                    <h5 className="card-title">"It Ends with Us" (Colleen Hoover - 2016)</h5>
-                                    <p className="card-text">A powerful and emotionally charged story that explores complex themes like love, resilience, and domestic abuse. A must-read for contemporary romance fans.</p>
-                                    <a href="/" className="btn btn-primary">Summary</a>
-                                </div>
+                            <img src={rs} className="card-img-top" alt="post1" />
+                            <div className="card-body">
+                                <h5 className="card-title">G20 Summit 2023: PM Rishi Sunak announces UK's biggest ever aid</h5>
+                                <p className="card-text">The United Kingdom contributed a record $2 billion to the Green Climate Fund to tackle climate change, Prime Minister Rishi Sunak announced on 10th Sep 2023.</p>
+                                <a href="/" className="btn btn-primary">Read More</a>
+                            </div>
                         </div>
                     </div>
-                     {/* Blog Post 2 */}
                     <div className="col-md-4">
                         <div className="card mb-4">
-                            <img src={ry} className="card-img-top" alt="post1"/>
-                                <div className="card-body">
-                                    <h5 className="card-title">"Regretting You" (Colleen Hoover - 2019)</h5>
-                                    <p className="card-text">A poignant exploration of family dynamics, loss, and love. Hoover beautifully portrays the bond between a mother and daughter.</p>
-                                    <a href="/" className="btn btn-primary">Read More</a>
-                                </div>
+                            <img src={pap} className="card-img-top" alt="post1" />
+                            <div className="card-body">
+                                <h5 className="card-title">"Pride and Prejudice" (Jane Austen - 1813)</h5>
+                                <p className="card-text">Timeless classic. Austen's wit and social commentary shine. Lively characters, love's evolution. Pride, prejudice, and a charming love story.</p>
+                                <a href="/" className="btn btn-primary">Read More</a>
+                            </div>
                         </div>
                     </div>
-                     {/* Blog Post 3 */}
                     <div className="col-md-4">
                         <div className="card mb-4">
-                            <img src={pap} className="card-img-top" alt="post1"/>
-                                <div className="card-body">
-                                    <h5 className="card-title">"Pride and Prejudice" (Jane Austen - 1813)</h5>
-                                    <p className="card-text">Timeless classic. Austen's wit and social commentary shine. Lively characters, love's evolution. Pride, prejudice, and a charming love story.</p>
-                                    <a href="/" className="btn btn-primary">Read More</a>
-                                </div>
+                            <img src={g20i} className="card-img-top" alt="post1" />
+                            <div className="card-body">
+                                <h5 className="card-title">G20 Summit 2023: Improved economic relations for India</h5>
+                                <p className="card-text">The G20 presidency provides a platform for India to engage with other countries and build stronger economic relationships, which could lead to increased trade, investment, and cooperation.</p>
+                                <a href="/" className="btn btn-primary">Read More</a>
+                            </div>
                         </div>
                     </div>
-                     {/* Blog Post 4 */}
                     <div className="col-md-4">
                         <div className="card mb-4">
-                            <img src="images/post1.jpg" className="card-img-top" alt="post1"/>
-                                <div className="card-body">
-                                    <h5 className="card-title">"Beautiful Bastard" (Christina Lauren - 2013)</h5>
-                                    <p className="card-text">This steamy workplace romance is a rollercoaster of passion and tension. The chemistry between the characters is scorching, making it a must-read for romance enthusiasts.</p>
-                                    <a href="/" className="btn btn-primary">Read More</a>
-                                </div>
+                            <img src={rk} className="card-img-top" alt="post1" />
+                            <div className="card-body">
+                                <h5 className="card-title">20 Best Places in Rishikesh</h5>
+                                <p className="card-text">Rishikesh is a place that is more than what meets the eye. While the majority of the tourists tend to visit this place because of extreme devotion and gain peace and solace.</p>
+                                <a href="/" className="btn btn-primary">Read More</a>
+                            </div>
                         </div>
                     </div>
-                     {/* Blog Post 5 */}
                     <div className="col-md-4">
                         <div className="card mb-4">
-                            <img src="images/post1.jpg" className="card-img-top" alt="post1"/>
-                                <div className="card-body">
-                                <h5 className="card-title">"The Love Hypothesis" (Helen Hoang - 2021)</h5>
-                                    <p className="card-text">A delightful contemporary romance that's quickly gained popularity. It's a charming and entertaining read.</p>
-                                    <a href="/" className="btn btn-primary">Read More</a>
-                                </div>
+                            <img src={ewu} className="card-img-top" alt="post1" />
+                            <div className="card-body">
+                                <h5 className="card-title">"It Ends with Us" (Colleen Hoover - 2016)</h5>
+                                <p className="card-text">A powerful and emotionally charged story that explores complex themes like love, resilience, and domestic abuse. A must-read for contemporary romance fans.</p>
+                                <a href="/" className="btn btn-primary">Read More</a>
+                            </div>
                         </div>
                     </div>
-                     {/* Blog Post 6 */}
                     <div className="col-md-4">
                         <div className="card mb-4">
-                            <img src="images/post1.jpg" className="card-img-top" alt="post1"/>
-                                <div className="card-body">
-                                    <h5 className="card-title">Blog Post Title 6</h5>
-                                    <p className="card-text"> is a heartbreaking and beautifully written tale of love, loss, and the power of human connection</p>
-                                    <a href="/" className="btn btn-primary">Read More</a>
-                                </div>
+                            <img src={wk} className="card-img-top" alt="post1" />
+                            <div className="card-body">
+                                <h5 className="card-title">West Kowloon Cultural District</h5>
+                                <p className="card-text">Creating Modern Traditions with Heritage and Craftsmanship. Hong Kong has over time emerged as one of the most popular tourist destinations across the world.</p>
+                                <a href="/" className="btn btn-primary">Read More</a>
+                            </div>
                         </div>
                     </div>
-                     {/* Blog Post 7 */}
+                    
                     <div className="col-md-4">
                         <div className="card mb-4">
-                            <img src="images/post1.jpg" className="card-img-top" alt="post1"/>
-                                <div className="card-body">
-                                    <h5 className="card-title">Blog Post Title 7</h5>
-                                    <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                                    <a href="/" className="btn btn-primary">Read More</a>
-                                </div>
+                            <img src={ic} className="card-img-top" alt="post1" />
+                            <div className="card-body">
+                                <h5 className="card-title">The Best Instant Pot Vegetarian Chili EVER!</h5>
+                                <p className="card-text">It's incredibly rich and hearty, super smoky and slightly spicy. Made with three types of beans, packed with veggies, it's family-friendly, ready in 30 minutes.</p>
+                                <a href="/" className="btn btn-primary">Read More</a>
+                            </div>
                         </div>
                     </div>
-                     {/* Blog Post 8 */}
                     <div className="col-md-4">
                         <div className="card mb-4">
-                            <img src="images/post1.jpg" className="card-img-top" alt="post1"/>
-                                <div className="card-body">
-                                    <h5 className="card-title">Blog Post Title 8</h5>
-                                    <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                                    <a href="/" className="btn btn-primary">Read More</a>
-                                </div>
+                            <img src={ry} className="card-img-top" alt="post1" />
+                            <div className="card-body">
+                                <h5 className="card-title">"Regretting You" (Colleen Hoover - 2019)</h5>
+                                <p className="card-text">A poignant exploration of family dynamics, loss, and love. Hoover beautifully portrays the bond between a mother and daughter.</p>
+                                <a href="/" className="btn btn-primary">Read More</a>
+                            </div>
                         </div>
                     </div>
-                     {/* Blog Post 9 */}
                     <div className="col-md-4">
                         <div className="card mb-4">
-                            <img src="images/post1.jpg" className="card-img-top" alt="post1"/>
-                                <div className="card-body">
-                                    <h5 className="card-title">Blog Post Title 9</h5>
-                                    <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                                    <a href="/" className="btn btn-primary">Read More</a>
-                                </div>
+                            <img src={vcc} className="card-img-top" alt="post1" />
+                            <div className="card-body">
+                                <h5 className="card-title">Vegan Carrot Cake With Vegan Frosting</h5>
+                                <p className="card-text">This dairy-free, lightened-up carrot cake is soft, moist, and beautifully sweet. There's plenty of cinnamon, allspice, and walnuts in every bite.</p>
+                                <a href="/" className="btn btn-primary">Read More</a>
+                            </div>
                         </div>
                     </div>
-                     {/* Blog Post 10 */}
-                    <div className="col-md-4">
-                        <div className="card mb-4">
-                            <img src="images/post1.jpg" className="card-img-top" alt="post1"/>
-                                <div className="card-body">
-                                    <h5 className="card-title">Blog Post Title 10</h5>
-                                    <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                                    <a href="/" className="btn btn-primary">Read More</a>
-                                </div>
-                        </div>
-                    </div>
-                    {/* Repeat for additional blog posts  */}
                 </div>
             </div>
         </section>
--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
         <header className="hero bg-primary text-white text-center py-5">
             <div className="container">
                 <h1>Welcome to My Blog</h1>
-                <p>This blog contains my reviews on my top 10 romantic books</p>
+                {/* <p>This blog contains my reviews on my top 10 romantic books</p> */}
             </div>
         </header>
     )
