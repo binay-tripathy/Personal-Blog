32dddbf6c18f4b9da738961fc2bb77dff740fc62
Binay Tripathy <binaytripathy123@gmail.com>
2023-09-29 17:35:20 +0530

    Slight Change
    Signed-off-by: Binay Tripathy <binaytripathy123@gmail.com>

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -5,9 +5,7 @@ const Navbar = () => {
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <div className="container">
                 <a className="navbar-brand" href="/">Personal Blog</a>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
+                
             </div>
         </nav>
     )
