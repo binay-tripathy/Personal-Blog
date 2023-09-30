44ffd62860a4e96fdcfe56dc5493bbe44336291b
Binay Tripathy <binaytripathy123@gmail.com>
2023-09-30 20:54:55 +0530

    Slight change to footer
    Signed-off-by: Binay Tripathy <binaytripathy123@gmail.com>

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
     return (
         <footer className="footer bg-dark text-white text-center py-3">
             <div className="container">
-                &copy; 2023 My Blog
+                &copy; 2023 <a href="http://www.github.com/binay-tripathy" className='text-light' target="_blank" rel="noopener noreferrer" style={{textDecoration:"none"}}> My Personal Blog</a>
             </div>
         </footer>
     )
--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -5,7 +5,6 @@ const Hero = (props) => {
         <header className="hero bg-primary text-white text-center py-5">
             <div className="container">
                 <h1>{props.name}</h1>
-                {/* <p>This blog contains my reviews on my top 10 romantic books</p> */}
             </div>
         </header>
     )
